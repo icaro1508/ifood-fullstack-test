ec621de73010dd84f1a1e94d7e146d670d8c9957
Ícaro Aguiar Perez <icaro.aguiar@oobj.com.br>
2020-09-20 15:54:58 -0300

    Versão do order-service que responde um mock para o api-gateway Versão do service-registry com as configurações corretas

--- order-service/src/main/java/com/ifood/order/OrderApplication.java
+++ order-service/src/main/java/com/ifood/order/OrderApplication.java
@@ -1,25 +1,11 @@
 package com.ifood.order;
 
-import com.ifood.order.document.Order;
 import com.ifood.order.event.OrderEventHandler;
-import com.ifood.order.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.ifood.order.repository")
---
+++ order-service/src/main/java/com/ifood/order/dto/OrderDto.java
@@ -0,0 +1,19 @@
+package com.ifood.order.dto;
+
+import com.ifood.order.document.Order;
+import lombok.Builder;
+import lombok.Data;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+public class OrderDto {
+	
+	private UUID id;
+	private final UUID clientId;
+	private final Date createdAt;
+	private final Date confirmedAt;
+	private final List<Order.Item> items;
+}
